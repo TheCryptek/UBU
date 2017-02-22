478d9105bb148c769b6d012958adebd653dbe898
TheCryptek <thecryptek@outlook.com>
2017-02-21 22:32:19 -0500

    Fixed some code and a few other things

--- UBU.py
+++ UBU.py
@@ -7,10 +7,12 @@ import traceback
 print ('Welcome to USB Backup Utility')
 print ('Created by: TheCryptek')
 print ('\nWhat directory would you like to back up?')
-print ('Example: C:/users/user/Desktop/Folder')
+print ('Windows: C:/users/user/Desktop/Folder')
+print ('Linux: /home/user/Desktop/Folder')
 backUp = raw_input('> ') # Files the user specified to back up
 print ('\nWhere would you like to back these files up at?')
-print ('Example USB Letter: E:/')
+print ('Windows: E:/')
+print ('Linux: /media/user/device')
 backDevice = raw_input('> ') # Device the user specified to save the back up on.
 print ('\nName of the zip file you prefer?')
 print ('Example: Backup.zip')
@@ -22,11 +24,12 @@ if not os.path.exists(backDevice + '/BackUp'): # If the BackUp folder doesn't ex
 	os.mkdir(backDevice + 'BackUp') # Make the backup folder on specified back up device
 
 bkZip = zipfile.ZipFile(backZip, 'w') # Not sure what to say for lines 26 - 31
-for dirname, subdirs, files in os.walk(backUp): 
+
+for dirname, subdirs, files in os.walk(backUp):
 	bkZip.write(dirname)
 	for filename in files:
 		bkZip.write(os.path.join(dirname, filename))
-	bkZip.close()
+bkZip.close()
 
 shutil.move(backZip, dest) # Moves the zip file from the work directory to the specified back up device.
 print('\nBackup finished, stored at ' + dest + '/' + backZip)
