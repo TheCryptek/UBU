a420abd3bdb4f2ab2bc4dbc88d6e685e3d7e424b
TheCryptek <thecryptek@outlook.com>
2017-02-21 22:40:38 -0500

    Removed shutil and correctly placed the file with zipfile

--- UBU.py
+++ UBU.py
@@ -12,7 +12,7 @@ print ('Linux: /home/user/Desktop/Folder')
 backUp = raw_input('> ') # Files the user specified to back up
 print ('\nWhere would you like to back these files up at?')
 print ('Windows: E:/')
-print ('Linux: /media/user/device')
+print ('Linux: /media/user/device/')
 backDevice = raw_input('> ') # Device the user specified to save the back up on.
 print ('\nName of the zip file you prefer?')
 print ('Example: Backup.zip')
@@ -23,7 +23,7 @@ dest = backDevice + '/BackUp'
 if not os.path.exists(backDevice + '/BackUp'): # If the BackUp folder doesn't exist on the device then
 	os.mkdir(backDevice + 'BackUp') # Make the backup folder on specified back up device
 
-bkZip = zipfile.ZipFile(backZip, 'w') # Not sure what to say for lines 26 - 31
+bkZip = zipfile.ZipFile(os.path.join(dest, backZip), 'w') # Not sure what to say for lines 26 - 31
 
 for dirname, subdirs, files in os.walk(backUp):
 	bkZip.write(dirname)
@@ -31,5 +31,4 @@ for dirname, subdirs, files in os.walk(backUp):
 		bkZip.write(os.path.join(dirname, filename))
 bkZip.close()
 
-shutil.move(backZip, dest) # Moves the zip file from the work directory to the specified back up device.
 print('\nBackup finished, stored at ' + dest + '/' + backZip)
