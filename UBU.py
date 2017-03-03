144623a3e442f2b5988a849c591ccdba604accfb
TheCryptek <thecryptek@outlook.com>
2017-03-02 22:20:50 -0500

    Re-wrote the script and cleaned the code up.

--- UBU.py
+++ UBU.py
@@ -1,37 +1,59 @@
 import os
 import sys
-import shutil
 import zipfile
 import traceback
 
-print ('Welcome to USB Backup Utility')
-print ('Created by: TheCryptek')
-print ('\nWhat directory would you like to back up?')
-print ('Windows: C:/users/user/Desktop/Folder')
-print ('Linux: /home/user/Desktop/Folder')
-backUp = raw_input('> ') # Files the user specified to back up
-print ('\nWhere would you like to back these files up at?')
-print ('Windows: E:/')
-print ('Linux: /media/user/device/')
-backDevice = raw_input('> ') # Device the user specified to save the back up on.
-print ('\nName of the zip file you prefer?')
-print ('Example: Backup.zip')
-backZip = raw_input('> ') # The name of the zip file specified by the user
-print ('\nBackup started...')
-dest = backDevice + '/BackUp'
+# The Welcome screen.
 
-if not os.path.exists(backDevice + '/BackUp'): # If the BackUp folder doesn't exist on the device then
-	os.mkdir(backDevice + 'BackUp') # Make the backup folder on specified back up device
+print """
+
+USB Backup Utility
+ _   _ ____  _   _ 
+| | | | __ )| | | |
+| | | |  _ \| | | |
+| |_| | |_) | |_| |
+ \___/|____/ \___/
+
+Created by TheCryptek
+"""
+
+# Obtain the directory the user wants to back up
+print "\nWhat directory would you like to back up?"
+print "Windows: C:/users/user/Desktop/folder"
+print "Linux: /home/user/Desktop/Folder"
+backUp = raw_input("> ")
+
+# Obtain the device the user wants to use
+print "\nWhere would you like to back these files up at?"
+print "Windows: E:/"
+print "Linux: /media/user/device/"
+backDevice = raw_input("> ")
+
+# Obtain the name of the zip file the user wants to use
+print "\nName of the zip file you prefer?"
+print "Example: Backup.zip"
+backZip = raw_input("> ")
+
+# Notify the user that the script has started.
+print "\nBackup started..."
+dest = backDevice + "/BackUp"
+
+# Check if the backup folder exists, if not, then create it.
+if not os.path.exists(backDevice + '/BackUp'):
+	os.mkdir(backDevice + "BackUp")
+
+# Check if the specified zip file exists, if so. Delete it.
 
 if os.path.exists(os.path.join(dest, backZip)):
 	os.remove(os.path.join(dest, backZip))
 
-bkZip = zipfile.ZipFile(os.path.join(dest, backZip), 'w') # Not sure what to say for lines 26 - 31
-
+# Create the zip file and place it in the user specified device.
+bkZip = zipfile.ZipFile(os.path.join(dest, backZip), 'w')
 for dirname, subdirs, files in os.walk(backUp):
 	bkZip.write(dirname)
 	for filename in files:
 		bkZip.write(os.path.join(dirname, filename))
 bkZip.close()
 
-print('\nBackup finished, stored at ' + dest + '/' + backZip)
+# Notify the user that the backup has finished and where it was saved.
+print "\nBackup finished, stored at " + dest + "/" + backZip
