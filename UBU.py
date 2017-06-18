01b90c50545023a91cc4805c5e27bda19bbc0291
TheCryptek <thecryptek@outlook.com>
2017-06-18 07:52:43 -0400

    Added bcolors class to the cryp module and implemented colors in UBU.py

--- UBU.py
+++ UBU.py
@@ -12,11 +12,21 @@ import zipfile
 import traceback
 import cryp
 
+# Make colors an easier way to type
+header = cryp.bcolors.HEADER
+blue = cryp.bcolors.OKBLUE
+green = cryp.bcolors.OKGREEN
+warning = cryp.bcolors.WARNING
+fail = cryp.bcolors.FAIL
+endc = cryp.bcolors.ENDC
+bold = cryp.bcolors.BOLD
+underline = cryp.bcolors.UNDERLINE
+
 cryp.clear()
 
 # The Welcome screen.
 
-print """
+print header + """
 
 USB Backup Utility
  _   _ ____  _   _ 
@@ -26,46 +36,48 @@ USB Backup Utility
  \___/|____/ \___/
 
 Created by TheCryptek
-"""
+""" + endc
 
 raw_input("Press enter to continue...")
 cryp.clear()
 
 # Obtain the directory the user wants to back up
-print "\nWhat directory would you like to back up?"
+print bold + "\nWhat directory would you like to back up?" + endc
 print "Windows: C:/users/user/Desktop/folder"
 print "Linux: /home/user/Desktop/Folder"
-backUp = raw_input("> ")
+backUp = raw_input(fail + "> " + endc)
 cryp.clear()
 
 # Obtain the device the user wants to use
-print "\nWhere would you like to back these files up at?"
+print bold + "\nWhere would you like to back these files up at?" + endc
 print "Windows: E:/"
 print "Linux: /media/user/device"
-backDevice = raw_input("> ")
+backDevice = raw_input(fail + "> " + endc)
 cryp.clear()
 
 # Obtain the name of the zip file the user wants to use
-print "\nName of the zip file you prefer?"
+print bold + "\nName of the zip file you prefer?" + endc
 print "Example: Backup.zip"
-backZip = raw_input("> ")
+backZip = raw_input(fail + "> " + endc)
 cryp.clear()
 
 # Notify the user that the script has started.
-print "\nBackup started..."
-dest = backDevice + "/BackUp"
+print green + "\nBackup started..."
+dest = backDevice + "/Backup"
 cryp.space()
-raw_input("Press enter to continue...")
-cryp.clear()
+
 
 # Check if the backup folder exists, if not, then create it.
-if not os.path.exists(backDevice + '/BackUp'):
-	os.mkdir(backDevice + "/BackUp")
+print warning + "Checking to see if " + dest + " exists."
+if not os.path.exists(backDevice + '/Backup'):
+	os.mkdir(backDevice + "/Backup")
+	print warning + dest + " did not exist... We have created it."
 
 # Check if the specified zip file exists, if so. Delete it.
-
+print warning + "Checking for " + backZip
 if os.path.exists(os.path.join(dest, backZip)):
 	os.remove(os.path.join(dest, backZip))
+	print warning + backZip + " existed, we have removed it to update it."
 
 # Create the zip file and place it in the user specified device.
 bkZip = zipfile.ZipFile(os.path.join(dest, backZip), 'w')
@@ -79,7 +91,7 @@ for dirname, subdirs, files in os.walk(backUp):
 bkZip.close()
 
 # Notify the user that the backup has finished and where it was saved.
-print "\nBackup finished, stored at " + dest + "/" + backZip
+print green + "\nBackup finished, stored at " + dest + "/" + backZip + endc
 cryp.space()
-raw_input("Press enter to continue...")
+raw_input("Press enter to finish...")
 cryp.clear()
